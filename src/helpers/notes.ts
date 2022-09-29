6d027c9973fbe278fddcc7c71dc9e06fc7c2ca87
Anton Sharudylo <a.sharudylo@gmail.com>
2022-09-29 15:11:17 +0300

    done

---
+++ src/helpers/notes.ts
@@ -0,0 +1,71 @@
+import { Note } from "../services/types";
+
+export const NotesData: Note[] = [
+  {
+    id: "1",
+    name: "Shopping list",
+    created: new Date(),
+    category: "Task",
+    content: "Lorem, ipsum.",
+    isArchived: true,
+  },
+  {
+    id: "2",
+    name: "Ipsum",
+    created: new Date(),
+    category: "Idea",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+    isArchived: true,
+  },
+  {
+    id: "3",
+    name: "Sit amet",
+    created: new Date(),
+    category: "Random Thought",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    isArchived: true,
+  },
+  {
+    id: "4",
+    name: "Adipiscing ",
+    created: new Date(),
+    category: "Task",
+    content: "Do eiusmod tempor",
+    isArchived: true,
+  },
+  {
+    id: "5",
+    name: "Ipsum",
+    created: new Date(),
+    category: "Idea",
+    content: "Lorem ipsum",
+    isArchived: true,
+  },
+  {
+    id: "6",
+    name: "Amet",
+    created: new Date(),
+    category: "Random Thought",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    isArchived: true,
+  },
+  {
+    id: "7",
+    name: "List",
+    created: new Date(),
+    category: "Task",
+    content: "Lorem, ipsum.",
+    isArchived: true,
+  },
+  {
+    id: "8",
+    name: "Archive",
+    created: new Date(),
+    category: "Random Thought",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    isArchived: false,
+  },
+];
---
+++ src/services/validationSchemas.ts
@@ -0,0 +1,55 @@
+import * as yup from "yup";
+
+const categoryNames = ["Task", "Random Thought", "Idea"];
+
+export const addNoteBodySchema = yup
+  .object({
+    name: yup.string().min(1).required("Please provide a name of note."),
+    category: yup
+      .string()
+      .oneOf(categoryNames, "Category must be: Task, Random Thought or Idea.")
+      .required("Please provide a category of note."),
+    content: yup.string().min(1).required("Please provide a content of note."),
+  })
+  .strict()
+  .noUnknown(true);
+
+yup.addMethod(yup.object, "atLeastOneOf", function (list: string[]) {
+  return this.test({
+    name: "atLeastOneOf",
+    message: `Object must have at least one of these keys: ${list.join(", ")}`,
+    exclusive: true,
+    params: { keys: list.join(", ") },
+    test: (value) => value == null || list.some((f) => value[f] != null),
+  });
+});
+
+export const editNoteBodySchema = yup
+  .object({
+    name: yup.string().min(1),
+    category: yup
+      .string()
+      .oneOf(categoryNames, "Category must be: Task, Random Thought or Idea"),
+    content: yup.string().min(1),
+    isArchived: yup.boolean(),
+  })
+  .atLeastOneOf(["name", "category", "content", "isArchived"])
+  .strict()
+  .noUnknown(true);
+
+export const uuidSchema = yup
+  .object({
+    id: yup
+      .string()
+      .required("Please provide a content the id of note.")
+      .uuid(),
+  })
+  .strict()
+  .noUnknown(true);
+
+export const emptySchema = yup.object({}).strict().noUnknown(true);
+
+export type AddNoteBodySchema = typeof addNoteBodySchema;
+export type EditNoteBodySchema = typeof editNoteBodySchema;
+export type UuidSchema = typeof uuidSchema;
+export type EmptySchema = typeof emptySchema;
